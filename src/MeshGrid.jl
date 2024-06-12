f8cf24b7d06be8d906c6421e9d5ea0017ecefd98
Jake W. Liu <126843050+akjake616@users.noreply.github.com>
2024-06-12 20:46:44 +0800

    Update MeshGrid.jl

--- src/MeshGrid.jl
+++ src/MeshGrid.jl
@@ -2,7 +2,7 @@ module MeshGrid
 
 export meshgrid
 
-function meshgrid(x::Union{Vector, StepRangeLen, UnitRange}, y::Union{Vector, StepRangeLen, UnitRange})
+function meshgrid(x::Union{Vector, StepRangeLen, StepRange, UnitRange}, y::Union{Vector, StepRangeLen, StepRange, UnitRange})
 
     sx = length(x)
     sy = length(y)
@@ -20,7 +20,7 @@ function meshgrid(x::Union{Vector, StepRangeLen, UnitRange}, y::Union{Vector, St
     return (X, Y)
 end
 
-function meshgrid(x::Union{Vector, StepRangeLen, UnitRange}, y::Union{Vector, StepRangeLen, UnitRange}, z::Union{Vector, StepRangeLen, UnitRange})
+function meshgrid(x::Union{Vector, StepRangeLen, StepRange, UnitRange}, y::Union{Vector, StepRangeLen, StepRange, UnitRange}, z::Union{Vector, StepRangeLen, StepRange, UnitRange})
     
     sx = length(x)
     sy = length(y)
